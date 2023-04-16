fa18d8b919d27a511b8154f7cc8a2b867ca74029
Chandan Sharma <chandan.srm05@gmail.com>
2023-04-16 16:43:03 +0530

    backend made
    done

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
-const app = express();
-const PORT = 8000;
+import Connection from './database/db.js';
+import dotenv from 'dotenv'
+import Router from './routes/route.js';
+dotenv.config();
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+const app = express();
+
+app.use('/', Router);
+
+const PORT = 8000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`)); 
+Connection(username,password);
+
